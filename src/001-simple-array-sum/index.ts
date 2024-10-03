cc8fbac7a78b8d43ae90f1f67529b6bfee8825e0
efrencodes <efrencodes@gmail.com>
2024-10-02 19:30:27 -0700

    feat: Config prettier

---
+++ src/001-simple-array-sum/index.ts
@@ -0,0 +1,12 @@
+(() => {
+    // Given an array of integers, find the sum of its elements.
+    const simpleArraySum = (ar: number[]): number => {
+        let sum: number = 0;
+        ar.map((number) => (sum += number));
+        return sum;
+    };
+
+    const ar = [1, 2, 3];
+
+    console.log(simpleArraySum(ar));
+})();
---
+++ src/002-compare-the-triplets/index.ts
@@ -0,0 +1,17 @@
+(() => {
+    const compareTriplets = (a: number[], b: number[]): number[] => {
+        let alice: number = 0;
+        let bob: number = 0;
+        const length: number = a.length;
+        for (let i = 0; i < length; i++) {
+            if (a[i] > b[i]) alice += 1;
+            if (a[i] < b[i]) bob += 1;
+        }
+        return [alice, bob];
+    };
+
+    const a = [1, 2, 3, 1];
+    const b = [3, 2, 2, 1];
+
+    console.log('compareTriplets => ', compareTriplets(a, b));
+})();
